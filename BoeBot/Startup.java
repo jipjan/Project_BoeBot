f7d46b869a4286e5cf08499531e2b805cb35e33d
Jaap-Jan de Wit <jipjan@gmail.com>
2016-11-22 10:07:57 +0100

    verwijderd nutteloze code

--- BoeBot/Startup.java
+++ BoeBot/Startup.java
@@ -8,11 +8,6 @@ public class Startup
 {
     public static void main(String[] args)
     {
-        DriveHandler handler = new DriveHandler();
 
-        while (true) 
-        {
-            handler.turn(200);
-        }
     }
 }
